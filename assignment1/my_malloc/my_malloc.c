bac4ec06d25f477f5fa6ff45ec78784195531b09
wrd233 <2608242024@qq.com>
2024-01-21 20:31:29 +0800

    能够跑通测试了

--- assignment1/my_malloc/my_malloc.c
+++ assignment1/my_malloc/my_malloc.c
@@ -1,59 +1,95 @@
 #include "my_malloc.h"
 #include <stdint.h>
+#include <unistd.h>
 
 typedef struct block block_t;
 struct block
 {
-    uint64_t header;            // block size + 状态 TODO: 这里的64位是为了支持64位机器，但是到32位机器上就可能浪费，而且这里的不统一可能导致无法对齐
+    // TODO: 如果要考虑对其，这里跨平台的不统一可能导致无法对齐
+    size_t payload_size; 
+    int is_allocated;           
     block_t* next; 
     block_t* prev;
     unsigned char payload[0];   // 真正提供出去的地址
 };
 
-
-size_t get_size(block_t* block_ptr){
-    // 提取高 61 位
-    size_t size = (size_t)(block_ptr->header >> 3);
-    return size;
-}
-
-int get_state(block_t* block_ptr){
-    return (block_ptr->header & 1) ? 1 : 0
-}
-
-
 static block_t* block_head = NULL;
 static block_t* free_head = NULL;
+unsigned long largest_free_size = 0;    // TODO: 添加这个的全局逻辑
+unsigned long free_size_sum = 0;        // TODO: 添加这个的全局逻辑
 
-// 使用first fit的策略找到合适的block
-block_t* find_fit_ff(){
-
+// TODO: 使用first fit的策略找到合适的block
+static block_t* find_fit_ff(size_t payload_size){
+    return NULL;
 }
 
-// 使用best fit的策略找到合适的block
-block_t* find_fit_bf(){
-
+// TODO: 使用best fit的策略找到合适的block
+static block_t* find_fit_bf(size_t payload_size){
+    return NULL;
 }
 
+// 如果原先的内存池不够了，那么就拓展制定大小的block
+static block_t* extend_heap(size_t payload_size){
+    size_t size_sum = sizeof(struct block) + payload_size;
+    block_t* new_ptr = sbrk(size_sum);
 
+    new_ptr->payload_size = payload_size;
+    new_ptr->is_allocated = 1;
 
+    if(block_head == NULL){
+        block_head = new_ptr;
+    }
+
+    return new_ptr;
+}
+
+// TODO: 收回内存的时候直接合并相邻的
+// TODO: 把新的空闲块加入到空闲链表当中(头插吧要不)
+static void block_free(block_t* block){
+    if(free_head == NULL){
+        free_head = block;
+    }
+}
 
 
 void * ff_malloc(size_t size){
-    // 1.
-    return NULL;
+    // 首先尝试从空闲链表中的空闲块中分配
+    block_t* block_ptr = find_fit_ff(size);
+
+    if(block_ptr == NULL){  // 此时空闲链表中没有合适的块，所以需要进行拓展
+        block_ptr = extend_heap(size);
+    }else{  // 从空闲链表中找到合适的块
+        // TODO: 从空闲链表中删除这一空闲块
+    }
+    return block_ptr;
 }
 
 void * bf_malloc(size_t size){
-    return NULL;
+    // 首先尝试从空闲链表中的空闲块中分配
+    block_t* block_ptr = find_fit_bf(size);
+
+    if(block_ptr == NULL){  // 此时空闲链表中没有合适的块，所以需要进行拓展
+        block_ptr = extend_heap(size);
+    }else{  // 从空闲链表中找到合适的块
+        // TODO: 从空闲链表中删除这一空闲块
+    }
+    return block_ptr;
 }
 
 
 void ff_free(void * ptr){
-
+    block_free(NULL);
 }
 
 
 void bf_free(void * ptr){
+    block_free(NULL);
+}
 
+unsigned long get_largest_free_data_segment_size() {
+  return largest_free_size;
+}
+
+unsigned long get_total_free_size() {
+  return free_size_sum;
 }
--- assignment1/my_malloc/my_malloc.h
+++ assignment1/my_malloc/my_malloc.h
@@ -15,4 +15,7 @@ void ff_free(void * ptr);
 //Best Fit free
 void bf_free(void * ptr);
 
+unsigned long get_largest_free_data_segment_size(); 
+unsigned long get_total_free_size();
+
 #endif /* MY_MALLOC_H */
